8d0df0372b29a83b9e6ddf9ec8052ab01076b545
XpG <1099477816@qq.com>
2020-04-02 15:45:08 +0800

    firstcommit time:2020/4/2/15:44 name:Xpg74138

--- ourfirstapp/src/main/java/com/example/ourfirstapp/MainActivity.java
+++ ourfirstapp/src/main/java/com/example/ourfirstapp/MainActivity.java
@@ -11,10 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG,"ONPAUSE");
-    }
 }
