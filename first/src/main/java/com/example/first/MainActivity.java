196be5e80762239265bae00466012418585368a1
XpG <1099477816@qq.com>
2020-04-08 16:58:32 +0800

    xpg 2020/4/8 16:57 homework

--- first/src/main/java/com/example/first/MainActivity.java
+++ first/src/main/java/com/example/first/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.renderscript.Sampler;
 import android.support.annotation.NonNull;
 import android.app.Fragment;
 import android.support.v4.app.TaskStackBuilder;
@@ -12,13 +13,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG="MAIN_ACTIVITY";
     TextView mTextView;
     private Fragment mFragmentOne;
     private Fragment mFragmentTwo;
+    private List<Map<String,Object>> mData;
+    private SimpleAdapter mSimpleleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,11 +104,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        ListView lv=findViewById(R.id.list_view);
+        mData=getData();
+        mSimpleleAdapter=new SimpleAdapter(
+                this,
+                mData,
+                R.layout.list_view_item,
+                new String[]{"img","title","http"},
+                new int[]{R.id.image_view,R.id.text_view_1,R.id.text_view_2});
+
+        lv.setAdapter(mSimpleleAdapter);
         Log.d(TAG,"ONRESUME");
+    }
+
+    private List<Map<String,Object>>getData() {
+        List<Map<String,Object>>list= new ArrayList<>();
+        Map<String,Object> map=new HashMap<>();
+        map.put("img",R.drawable.ic_launcher_background);
+        map.put("title","Grossini");
+        map.put("http","google.com");
+
+        list.add(map);
+
+        map=new HashMap<>();
+        map.put("img",R.drawable.ic_launcher_background);
+        map.put("title","Hello");
+        map.put("http","This is the second");
+
+        list.add(map);
+        return list;
     }
 
     @Override
     protected void onPause() {
+
+
+
         super.onPause();
         Log.d(TAG,"ONPAUSE");
     }
